542525c699bef7a3cc515742fa963613acf4ddad
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-11-12 14:57:11 +0530

    day 30th started with node.js

---
+++ js30th.js
@@ -0,0 +1,14 @@
+// let n = 5;
+// for (let i = 0; i<n; i++){
+//     console.log("Bakend", i );
+// };
+
+// console.log("started");
+
+// console.log(process.argv);
+
+// -------------------------------------------------------
+
+const someValue = require("./math");/**this will inport the file form the math.js */
+
+console.log(someValue);/**this will fetch the value form the math.js */
---
+++ math.js
@@ -0,0 +1,7 @@
+const sum = (a,b) => a + b;   
+const mul = (a , b) => a * b;
+//  require() a built-in function to include external modules that exits in separete fies.
+// module.exports - a special object id used to export.
+
+module.exports = 123;/*this will export to any file which neede*/
+
